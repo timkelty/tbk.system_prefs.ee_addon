34413dcf4710cfd4a69052ff44f86ab2b7ee8f36
Tim Kelty <timkelty@gmail.com>
2009-06-19 12:18:53 -0400

    adding plugin

---
+++ system/plugins/pi.tk_system_prefs.php
@@ -0,0 +1,39 @@
+<?php
+
+$plugin_info = array(
+						'pi_name'			=> 'TK System Prefs',
+						'pi_version'		=> '0.1.0',
+						'pi_author'			=> 'Tim Kelty and Karl Swedberg',
+						'pi_author_url'		=> 'http://geniuscar.com/docs and http://www.learningjquery.com',
+						'pi_description'	=> 'Use system preferences as variables.',
+						'pi_usage'			=> Tk_system_prefs::usage()
+					);
+
+class Tk_system_prefs {
+	var $return_data = '';
+	
+	function Tk_system_prefs( $str = '' )
+	{
+		global $FNS, $TMPL, $PREFS;
+		
+		$this->return_data = '';
+		if ($str == '')
+		{
+        	$str = $TMPL->tagdata;
+		}
+		
+		// gently lifted from Leevi Graham's LG Add Site Name
+		foreach($PREFS->core_ini as $key => $value) {
+      if(is_array($value) === FALSE && strpos($str, LD . $key . RD) !== FALSE) {
+       $str = str_replace(LD.$key.RD, $value, $str);
+      }
+		}
+		$this->return_data = $str;
+	}
+	
+	function usage()
+	{
+		return "{exp:tk_system_prefs}....{/exp:tk_system_prefs}";
+	}
+	
+}
